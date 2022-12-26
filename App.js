3f0c51531d99c094ef0cfed2f338519736b05814
Tejas Itankar <tejasitankar94820@gmail.com>
2022-12-26 19:11:09 +0530

    loaded images from firestorage and created a database complete in home tab

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import SplashScreen from 'react-native-splash-screen';
@@ -8,7 +8,9 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from './components/Home';
 import Categories from './components/Categories';
 import Favourites from './components/Favourites';
-// import {NativeBaseProvider} from "native-base";
+import {NativeBaseProvider} from "native-base";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 firestore()
   .collection('Users')
   .add({
@@ -22,19 +24,39 @@ firestore()
 const Tab = createBottomTabNavigator();
 
 const App = () => {
+
   useEffect(() => {
     SplashScreen.hide();
   }, []);
+
   return (
-    // <NativeBaseProvider>
+    <NativeBaseProvider>
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="Categories" component={Categories} />
-          <Tab.Screen name="Favourites" component={Favourites} />
+          <Tab.Screen name="Home" component={Home}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+        }}/>
+          <Tab.Screen name="Categories" component={Categories} 
+          options={{
+            tabBarLabel: 'Categories',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="magnify" color={color} size={size} />
+            ),
+          }}/>
+          <Tab.Screen name="Favourites" component={Favourites} 
+          options={{
+            tabBarLabel: 'Favourites',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="heart" color={color} size={size} />
+            ),
+          }}/>
         </Tab.Navigator>
       </NavigationContainer>
-    // </NativeBaseProvider>
+    </NativeBaseProvider>
   );
 };
 const styles = StyleSheet.create({
--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,51 @@
-// import {Box} from 'native-base';
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Box} from 'native-base';
+import React, { useEffect, useState } from 'react';
+import {FlatList, Image, StyleSheet, Text, View} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 function Home(){
+
+  const [links,setLinks] = useState([])
+  const [name,setName] = useState('')
+  
+  const getData = async () => {
+    const data = await firestore().collection('Actress').doc('rVDZ6MNnVGah9aDijS6v').get();
+    // console.log(data._data)
+    setName(data._data.name)
+    setLinks(data._data.links)
+  }
+  
+  useEffect(() => {
+    getData()
+  }, []);
+
+  console.log(name)
+  console.log(links[0])
+
   return (
-    <View>
-      <Text style={styles.text}>Hello world</Text>
-    </View>
+    <Box flex={1} bg="#fff" alignItems="center" justifyContent="center" >
+      <Text>Home Screen</Text>
+        <FlatList data={links}
+          renderItem={({item}) => <Image source={{uri:item.url}} style = {{ width: 200, height: 200 }}/>}
+        />
+        
+        {/* <Text></Text> */}
+      {/* <FlatList
+        data={[
+          {key: 'Devin'},
+          {key: 'Dan'},
+          {key: 'Dominic'},
+          {key: 'Jackson'},
+          {key: 'James'},
+          {key: 'Joel'},
+          {key: 'John'},
+          {key: 'Jillian'},
+          {key: 'Jimmy'},
+          {key: 'Julie'},
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      /> */}
+    </Box>
   );
 }
 const styles = StyleSheet.create({
